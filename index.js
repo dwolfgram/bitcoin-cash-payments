e199de8b968952de937b8aa2264be39bb351fd3b
dwolfgram <daniel.j.wolfgram@gmail.com>
2019-05-14 01:25:45 -0400

    axios default

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const axios = require('axios')
+const axios = require('axios').default
 const bch = require('bitcoincashjs')
 const bchaddr = require('bchaddrjs')
 const request = require('request')
